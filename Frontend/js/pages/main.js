385ade359bf25ac3e0f28ebe1ca9654bd358eed7
Dimitris Christodoulou <dimitris.christodoulou.dev@gmail.com>
2017-07-21 22:39:39 +0300

    Minor changes
    Must create the Agents page

--- Frontend/js/pages/main.js
+++ Frontend/js/pages/main.js
@@ -8,9 +8,6 @@ $(document).ready(function () {
     $('#contactsTabs.tabs').tabs({
         swipeable: true
     });
-    $('.carousel').css('height: 500px');
-    // $('.tabs-content').removeClass('carousel initialized');
-
 
 //    $('selector').on('event', functionNameOut of document.ready)
 
